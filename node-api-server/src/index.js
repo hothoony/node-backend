e376e2b299fb5d9ce60346ac3dbc26f615534f57
hothoony <hothoony@hanmail.net>
2023-03-27 13:56:13 +0900

    .

--- node-api-server/src/index.js
+++ node-api-server/src/index.js
@@ -63,6 +63,7 @@ app.patch('/members/:id', (req, res) => {
     res.status(200).json({
         message: 'patch members ok',
         parseParam: {
+            id: req.query.id,
             name: req.body.name,
             age: req.body.age,
         },
