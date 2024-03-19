9fa7230b32798b27fdb4a87ae6b449c3ed1a8405
hothoony <hothoony@hanmail.net>
2024-03-19 10:06:09 +0900

    .

--- node-rest-api/src/index2.js
+++ node-rest-api/src/index2.js
@@ -7,13 +7,12 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 
 const teamRoute = require('./routes/teamRoute');
-const memberRoute = require('./routes/memberRoute');
-const { isEmpty } = require('./utils/util');
-const authInterceptor = require('./interceptors/authInterceptor');
-const logInterceptor = require('./interceptors/logInterceptor');
 
 const app = express();
-const router = express.Router();
+
+const ads = [
+    {title: 'Hello world 123'}
+];
 
 // middleware
 app.use(helmet());
@@ -23,15 +22,79 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 app.use(morgan('combined'));
 
-// interceptor
-app.use(logInterceptor);
-app.use(authInterceptor);
+app.use('/api/v1/teams', teamRoute);
+// app.use(teamRoute);
+// app.use('/users', userRoute);
 
-// route
-app.use('/api/v1', memberRoute);
-app.use('/api/v1', teamRoute);
+// routes
+app.get('/', (req, res) => {
+    res.send(ads);
+});
 
-// port
+// controller
+app.get('/members', (req, res) => {
+    console.log('');
+    console.log('## get members');
+
+    // header
+    const authorization = req.header('Authorization');
+    console.log('  authorization =', authorization);
+
+    // query string
+    console.log('  query page =', req.query.page);
+    console.log('  query size =', req.query.size);
+
+    res.status(200).json({
+        message: 'get members ok',
+        parseParam: {
+            header: req.header('Authorization'),
+            page: req.query.page,
+            size: req.query.size,
+        },
+    });
+});
+
+app.patch('/members/:id', (req, res) => {
+    console.log('');
+    console.log('## patch members');
+
+    // path variable
+    console.log('  req.params.id =', req.params.id);
+
+    // form data
+    // Content-Type: application/x-www-form-urlencoded
+    console.log('  req.body.name =', req.body.name);
+    console.log('  req.body.age =', req.body.age);
+
+    res.status(200).json({
+        message: 'patch members ok',
+        parseParam: {
+            id: req.params.id,
+            name: req.body.name,
+            age: req.body.age,
+        },
+    });
+});
+
+app.post('/members', (req, res) => {
+    console.log('');
+    console.log('## post members');
+
+    // Content-Type: application/json
+    console.log('  req.body.name =', req.body.name);
+    console.log('  req.body.age =', req.body.age);
+    console.log('  req.body.gender =', req.body.gender);
+
+    res.status(200).json({
+        message: 'post members ok',
+        parseParam: {
+            name: req.body.name,
+            age: req.body.age,
+            gender: req.body.gender,
+        },
+    });
+});
+
 const SERVER_PORT = 3001;
 
 app.listen(SERVER_PORT, () => {
--- node-rest-api/src/routes/teamRoute.js
+++ node-rest-api/src/routes/teamRoute.js
@@ -1,16 +1,9 @@
 const express = require('express');
-<<<<<<< HEAD
-const { findAll } = require('../controller/teamController');
 
-const router = express.Router();
-
-router.get('/', findAll);
-=======
 const teamController = require('../controllers/teamController');
 
 const router = express.Router();
 
 router.get('/teams', teamController.findAll);
->>>>>>> a436adcf7d24cca071a6caf54484c3da3685cab7
 
 module.exports = router;
