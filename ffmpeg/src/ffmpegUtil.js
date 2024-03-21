5f4d1753e0ec68078a5fce0d355684fa9ef05059
hothoony <hothoony@hanmail.net>
2024-03-21 16:42:11 +0900

    .

--- ffmpeg/src/ffmpegUtil.js
+++ ffmpeg/src/ffmpegUtil.js
@@ -75,30 +75,70 @@ const ffmpeg = require('fluent-ffmpeg');
 //     })
 // ;
 
-// 동영상에서 썸네일 이미지 추출
-ffmpeg()
-    .input('./files/original.mp4')
-    // .screenshots({
-    //     timestamps: [20, 40, 60, 80, 100], // 초를 입력
-    //     filename: 'thumbnail-%i-%s.png',
-    //     folder: './files',
-    //     size: '320x240',
-    // })
-    .screenshots({
-        count: 3,
-        filename: 'thumbnail-%i-%s.png',
-        folder: './files',
-        size: '320x240',
-    })
-    .on('progress', (progress) => {
-        if (progress.percent) {
-            console.log(`processing ${progress.percent}`);
-        }
-    })
-    .on('end', () => {
-        console.log('end');
-    })
-    .on('error', (err) => {
-        console.log('err', err);
-    })
-;
+// // 동영상에서 썸네일 이미지 추출
+// ffmpeg()
+//     .input('./files/original.mp4')
+//     // .screenshots({
+//     //     timestamps: [20, 40, 60, 80, 100], // 초를 입력
+//     //     filename: 'thumbnail-%i-%s.png',
+//     //     folder: './files',
+//     //     size: '320x240',
+//     // })
+//     .screenshots({
+//         count: 3,
+//         filename: 'thumbnail-%i-%s.png',
+//         folder: './files',
+//         size: '320x240',
+//     })
+//     .on('progress', (progress) => {
+//         if (progress.percent) {
+//             console.log(`processing ${progress.percent}`);
+//         }
+//     })
+//     .on('end', () => {
+//         console.log('end');
+//     })
+//     .on('error', (err) => {
+//         console.log('err', err);
+//     })
+// ;
+
+// // 동영상 효과
+// ffmpeg()
+//     .input('./files/original.mp4')
+//     .videoFilters('fade=in:0:100')
+//     .saveToFile('./files/filter.mp4')
+//     .on('progress', (progress) => {
+//         if (progress.percent) {
+//             console.log(`processing ${progress.percent}`);
+//         }
+//     })
+//     .on('end', () => {
+//         console.log('end');
+//     })
+//     .on('error', (err) => {
+//         console.log('err', err);
+//     })
+// ;
+
+// 동영상 metadata
+ffmpeg.ffprobe('./files/original.mp4', function(err, metadata) {
+    console.log('metadata', metadata);
+    console.log();
+    console.log('format.filename', metadata.format.filename);
+    console.log('format.duration', metadata.format.duration);
+    console.log('format.size', metadata.format.size);
+    console.log();
+    console.log('streams[0].codec_type', metadata.streams[0].codec_type);
+    console.log('streams[0].codec_name', metadata.streams[0].codec_name);
+    console.log('streams[0].width', metadata.streams[0].width);
+    console.log('streams[0].height', metadata.streams[0].height);
+    console.log('streams[0].display_aspect_ratio', metadata.streams[0].display_aspect_ratio);
+    console.log('streams[0].duration', metadata.streams[0].duration);
+    console.log('streams[0].duration_ts', metadata.streams[0].duration_ts);
+    console.log();
+    console.log('streams[1].codec_type', metadata.streams[1].codec_type);
+    console.log('streams[1].codec_name', metadata.streams[1].codec_name);
+    console.log('streams[1].duration', metadata.streams[1].duration);
+    console.log('streams[1].duration_ts', metadata.streams[1].duration_ts);
+});
